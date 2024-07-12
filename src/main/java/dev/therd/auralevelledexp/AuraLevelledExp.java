7a66d05173b895643a7498bf512b806126ded065
ant <antonio.dioquino@hotmail.com>
2024-07-13 02:30:40 +0800

    Initial Commit

---
+++ src/main/java/dev/therd/auralevelledexp/AuraLevelledExp.java
@@ -0,0 +1,17 @@
+package dev.therd.auralevelledexp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AuraLevelledExp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
