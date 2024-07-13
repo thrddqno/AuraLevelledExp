079fe3fe18c70713cba43ab4a5e85007c3f4c126
ant <antonio.dioquino@hotmail.com>
2024-07-13 14:39:34 +0800

    Create plugin commands

--- src/main/java/dev/therd/auralevelledexp/AuraLevelledExp.java
+++ src/main/java/dev/therd/auralevelledexp/AuraLevelledExp.java
@@ -1,17 +1,29 @@
 package dev.therd.auralevelledexp;
 
+import dev.therd.auralevelledexp.commands.AuraLevelledExpCommands;
 import org.bukkit.plugin.java.JavaPlugin;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import dev.therd.auralevelledexp.config.ConfigManager;
 
 public final class AuraLevelledExp extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).silentLogs(true));
+    }
 
+    @Override
+    public void onEnable() {
+        CommandAPI.onEnable();
+        ConfigManager.getInstance().setPlugin(this);
+        getLogger().info("Plugin enabled.");
+        ConfigManager.getInstance().getConfig("config.yml");
+        AuraLevelledExpCommands.registerCommands();
+        getLogger().info("Commands registered.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/dev/therd/auralevelledexp/commands/AuraLevelledExpCommands.java
@@ -0,0 +1,30 @@
+package dev.therd.auralevelledexp.commands;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
+import dev.therd.auralevelledexp.config.ConfigManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.checkerframework.checker.units.qual.C;
+
+public class AuraLevelledExpCommands {
+    public static void registerCommands(){
+        // Reload configuration command
+        new CommandAPICommand("auralevelledmobs")
+                .withAliases("ale", "alexp", "lxp")
+                .withSubcommand(new CommandAPICommand("reload")
+                        .withPermission(CommandPermission.OP)
+                        .executes((sender, args) -> {
+                            ConfigManager.getInstance().reloadConfigs();
+                            sender.sendMessage("Configuration reloaded.");
+                        })
+                )
+                .withSubcommand(new CommandAPICommand("get")
+                        .withSubcommand(new CommandAPICommand("levelled_skills_gain")
+                                .executes((sender, args) -> {
+                                    sender.sendMessage(String.valueOf(ConfigManager.getInstance().getBoolean(ConfigManager.getInstance().getConfig("config.yml"), "levelled_skills_gain.enabled")));
+                                })
+                        )
+                )
+                .register();
+    }
+}
